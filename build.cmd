babebb98a574670ad2fb7e8e6497c6248bee31c7
AndriusK <andrius.k@sanitex.eu>
2017-02-17 10:16:55 +0200

    build batch

--- build.cmd
+++ build.cmd
@@ -1,1 +1,6 @@
-call msbuild SharpSSH.sln
+
+set msbuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+@if not exist %msbuild% @set msbuild="%ProgramFiles%\MSBuild\15.0\Bin\MSBuild.exe" 
+
+%msbuild% "/v:m" SharpSSH.sln
